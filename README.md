0270f333042fac467fba797fa6c0492165dc5af8
paulwritescode <paulkingdev7@gmail.com>
2024-01-30 21:26:07 +0300

    Login page Templated

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Dolphin-User-management-System
 
-Flask Project to manages users
+Flask Project to manage users
 
+_Technologies used_
 Language used = python
 framework = flask
+Bootstrap 5
--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+# __________________________MAIN INDEX LOGIN
 @app.route("/")
-def adminIndex():
-    return render_template('admin/index.html')
+def index():
+    return render_template('indexMain.html')
 
-if --__name__="__main__":
+
+# ____________________________________ADMIN LOGIN
+@app.route("/admin/")
+def adminIndex():
+    return render_template('admin/index.html', title ="Admin Login")
+
+
+# _____________________User Area_____________________________
+@app.route("/user/")
+def userIndex():
+    return render_template('User/index.html', title ="User Login")
+
+
+
+
+if __name__=="__main__":
     app.run(debug=True)
