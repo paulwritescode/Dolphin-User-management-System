2bb43c5dc696af8d8824a82ef580aaa39cabaf24
paulwritescode <paulkingdev7@gmail.com>
2024-02-07 00:19:41 +0300

    tuesday (user-registration)

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URi'] = "sqlite:///ums.sqlite"
+app.config['SQLALCHEMY_DATABSE_URI'] = "sqlite:///ums.sqlite"
 app.config["SECRET_KEY"] = ""
-db = SQLAlchemy(app)
-
-# User class
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    fname = db.C
+db = SQLAlchemy
 
 
 # MAIN INDEX LOGIN
 @app.route("/")
 def index():
     return render_template('indexMain.html')
-
 # ADMIN LOGIN
 @app.route("/admin/")
 def adminIndex():
@@ -27,11 +22,11 @@ def adminIndex():
 @app.route("/user/")
 def userIndex():
     return render_template('User/index.html', title ="User Login")
-
 # register
 @app.route("/user/signup")
 def userSignUp():
     return render_template('User/signup.html', title ="User signup")
+
 
 if __name__=="__main__":
     app.run(debug=True)
